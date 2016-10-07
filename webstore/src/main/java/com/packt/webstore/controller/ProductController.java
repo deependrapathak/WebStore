f1a65777126396fcd794e9705df6d2f0b67f2256
deependrapathak <deependrarajpathak@gmail.com>
2016-10-07 00:51:19 -0500

    handler mappings in controller

--- webstore/src/main/java/com/packt/webstore/controller/ProductController.java
+++ webstore/src/main/java/com/packt/webstore/controller/ProductController.java
@@ -1,25 +1,56 @@
 package com.packt.webstore.controller;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.MatrixVariable;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.packt.webstore.domain.Product;
+import com.packt.webstore.service.ProductService;
+
 
 @Controller
+@RequestMapping("/products")
 public class ProductController {
-	@RequestMapping("/products")
+	@Autowired
+	private ProductService productService;
+	
+	@RequestMapping()
 	public String list(Model model){
-		Product iphone=new Product("p1234","iphone 5s", new BigDecimal(500));
-		iphone.setDescription("Apple iPhone 5s smartphone with 4.00-inch 640x1136 display and 8-megapixel rear camera");
-				iphone.setCategory("Smart Phone");
-				iphone.setManufacturer("Apple");
-				iphone.setUnitsInStock(1000);
-				model.addAttribute("product", iphone);
-				return "products";
+	model.addAttribute("products", productService.getAllProducts());
+		return "products";
 		
 	}
+	@RequestMapping("/all")
+	public String allProduct(Model model){
+		model.addAttribute("products", productService.getAllProducts());
+		return "products";
+	}
+	@RequestMapping("/{category}")
+	public String getProductsByCategory(Model model,@PathVariable("category") String productCategory){
+		model.addAttribute("products", productService.getProductsByCategory(productCategory));
+		return "products";
+	}
+	
+	@RequestMapping("/product")
+	public String getProductById(@RequestParam("id") String productId,Model model){
+		
+		model.addAttribute("product", productService.getProductById(productId));
+		return "product";
+	}
+	@RequestMapping("/filter/{ByCriteria}")
+	public String getProductsByFilter(@MatrixVariable(pathVar="ByCriteria") Map<String, List<String>> filterParams,Model model){
+		model.addAttribute("products", productService.getProductsByFilter(filterParams));
+		return "products";
+	}
+	
+	
 
 }
--- webstore/src/main/webapp/WEB-INF/views/products.jsp
+++ webstore/src/main/webapp/WEB-INF/views/products.jsp
@@ -2,6 +2,7 @@
 	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="spring" %>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html;
@@ -22,6 +23,7 @@ min.css">
 	</section>
 	<section class="container">
 	<div class="row">
+	<c:forEach items="${products}" var="product">
 		<div class="col-sm-6 col-md-3" style="padding-bottom: 15px">
 			<div class="thumbnail">
 				<div class="caption">
@@ -29,9 +31,16 @@ min.css">
 					<p>${product.description}</p>
 					<p>${product.unitPrice}USD</p>
 					<p>Available ${product.unitsInStock} units in stock</p>
+					<p>
+					<a href="<spring:url value="/products/product?id=${product.productId}" />" class="btn btn-primary">
+					<span class="glyphicon-info-sign glyphicon"/></span> Details
+					</a>
+					
+					</p>				
 				</div>
 			</div>
 		</div>
+		</c:forEach>
 	</div>
 	</section>
 
